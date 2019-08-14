80cd9335ed22ea1b53ef5aa2c79a630c4d18d501
Sebastinez <sebastinez@me.com>
2019-08-14 07:57:57 -0300

    Add flags to world time

--- pages/horarios.php
+++ pages/horarios.php
@@ -4,44 +4,44 @@
 <div class="secctionfotos" id="proyectoprimary"></div>
 
   <div class="body-container-header" id="text">
-    <?php if ($lang === "en") { echo "World Clock";} else { echo "Horarios Mundiales"; } ?>
+    <?php if ($lang === "en") { echo "World Time";} else { echo "Horarios Mundiales"; } ?>
 </div>
   <div class="body-container-text">
   <div id="relojes">
       <div class="reloj-ind">
-        <div>Buenos Aires</div>
+        <div><img src="../img/flags/ar.svg" height="35px"><p>Buenos Aires</p></div>
         <div class="reloj +0" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>San Pablo</div>
+        <div><img src="../img/flags/br.svg" height="35px"><p>San Pablo</p></div>
         <div class="reloj +0" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>México DF</div>
+        <div><img src="../img/flags/mx.svg" height="35px"><p>México DF</p></div>
         <div class="reloj -2" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>Houston</div>
+        <div><img src="../img/flags/us.svg" height="35px"><p>Houston</p></div>
         <div class="reloj -2" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>Calgary</div>
+        <div><img src="../img/flags/ca.svg" height="35px"><p>Calgary</p></div>
         <div class="reloj -3" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>Londres</div>
+        <div><img src="../img/flags/gb.svg" height="35px"><p>Londres</p></div>
         <div class="reloj +4" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>Berlín</div>
+        <div><img src="../img/flags/de.svg" height="35px"><p>Berlín</p></div>
         <div class="reloj +5" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>Moscú</div>
+        <div><img src="../img/flags/ru.svg" height="35px"><p>Moscú</p></div>
         <div class="reloj +6" id="buenos_aires"></div>
       </div>
       <div class="reloj-ind">
-        <div>Shanghái</div>
+        <div><img src="../img/flags/cn.svg" height="35px"><p>Shanghái</p></div>
         <div class="reloj +11" id="buenos_aires"></div>
       </div>
 

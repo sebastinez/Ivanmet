dd21b6de0ee41911c72316e9b5e3e8f6e6feb70b
Sebastinez <sebastinez@me.com>
2019-08-27 07:59:20 -0300

    Add Image CRUD

--- cpanel/api_images/add_images.php
+++ cpanel/api_images/add_images.php
@@ -1,12 +1,4 @@
 <?php
- $target_dir = "../../expertise_imagenes/";
-$target_file = $target_dir . basename(hash_file("sha256",$_FILES["image"]["tmp_name"]).".jpg");
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
-} else {
-    echo "Sorry, there was an error uploading your file.";
-}
 
 ?>

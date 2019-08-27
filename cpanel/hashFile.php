705a426572226d2af6939ecca955efb1a65bc341
Sebastinez <sebastinez@me.com>
2019-08-27 15:02:37 -0300

    Refactor image Crud

--- cpanel/hashFile.php
+++ cpanel/hashFile.php
@@ -1,14 +1,14 @@
 <?php
 try {
 $fp = fopen("./file.json","w+");
-$dir = scandir("../expertise_imagenes");
+$dir = scandir("../expertise_imagenes_2");
 fwrite($fp,"{");
 foreach ($dir as $item) {
   if(strpos($item,"jpg")) {
-    $hash = hash_file("sha256","../expertise_imagenes/".$item);
+    $hash = hash_file("sha256","../expertise_imagenes_2/".$item);
     $string = '"'.$hash.'":"'.trim(substr($item,0,-4)).'",';
     fwrite($fp,$string);
-    rename("../expertise_imagenes/".$item,"../expertise_imagenes/".$hash.".jpg");
+    rename("../expertise_imagenes_2/".$item,"../expertise_imagenes_2/".$hash.".jpg");
   }
 }
 fwrite($fp,"}");

1a129555f75ac1a1f879c630397d63355fab1c88
Sebastinez <sebastinez@me.com>
2019-08-27 15:02:37 -0300

    Refactor image Crud

--- cpanel/pages/images.php
+++ cpanel/pages/images.php
@@ -8,7 +8,7 @@
 
 
 <div class="jumbotron" style="padding-top:30px; padding-bottom:30px;">
-<form action="./api_images/add_images.php" method="POST" enctype="multipart/form-data">
+<form action="./api_images/api_images.php" method="POST" enctype="multipart/form-data">
   <h1>Agregar imagen de background</h1>
   <div class="form-group">
     <label for="exampleInputEmail1">Titulo de archivo</label>
@@ -17,7 +17,7 @@
   <div class="form-group">
     <input type="file" class="form-control-file" id="file" name="image">
   </div>
-  <button type="submit" class="btn btn-primary">Subir imagen</button>
+  <button type="submit" name="add" class="btn btn-primary">Subir imagen</button>
 </form>
 </div>
 
@@ -30,7 +30,7 @@ foreach ($json_data as $key => $value) {
 ?>
 <div class='col-xs-12 col-md-6 col-lg-4'>
 
-  <img src='../expertise_imagenes/<?php echo $key; ?>.jpg' alt="<?php echo $value; ?>" class="p-4" style="width:100%;">
+  <img src='../expertise_imagenes_2/<?php echo $key; ?>.jpg' alt="<?php echo $value; ?>" class="p-4" style="width:100%;">
   <p class="px-4"><?php echo $value; ?></p>
   <button type="button" class="delete" data-toggle="modal" data-target="#borrar_<?php echo $key; ?>">
 Borrar
@@ -38,7 +38,7 @@ Borrar
   
 <!-- Button trigger modal -->
 <button type="button" class="edit" data-toggle="modal" data-target="#editar_<?php echo $key; ?>">
-Editar
+Editar titulo
 </button>
 </form>
 </div>
@@ -53,7 +53,7 @@ Editar
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form action="./api_images/edit_images.php" method="POST">
+      <form action="./api_images/api_images.php" method="POST">
 
       <div class="modal-body">
       Por favor modificar titulo de imagen
@@ -62,7 +62,7 @@ Editar
       <div class="modal-footer">
         <input type="hidden" value="<?php echo $key; ?>" name="hash">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-        <button type="submit" name="edit" value="edit" class="btn btn-primary">Guardar cambios</button>
+        <button type="submit" name="edit" class="btn btn-primary">Guardar cambios</button>
       </div>
       </form>
 
@@ -83,13 +83,13 @@ Editar
       <div class="modal-body">
         Por favor confirmar la eliminación de la siguiente imagen. 
         <h5 style="margin-top:15px;"><?php echo $value; ?></h5>
-        <img src="../expertise_imagenes/<?php echo $key; ?>.jpg" width="100%" style="margin-top:15px;">
+        <img src="../expertise_imagenes_2/<?php echo $key; ?>.jpg" width="100%" style="margin-top:15px;">
       </div>
       <div class="modal-footer">
-        <form action="./api_images/delete_images.php" method="GET">
+        <form action="./api_images/api_images.php" method="POST">
         <input type="hidden" value="<?php echo $key; ?>" name="hash">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-        <button type="submit" class="btn btn-primary">Borrar</a>
+        <button type="submit" name="delete" class="btn btn-primary">Borrar</a>
         </form>
       </div>
     </div>

df7511b7264580707869edac4208598ced73b40d
Sebastian <sebastinez@me.com>
2019-10-02 23:13:09 -0300

    Prettyfied

--- footer.php
+++ footer.php
@@ -1,25 +1,37 @@
 <div class="footer">
     <div class="footer-items">
-      <div><a href="?page=privacidad" style="text-decoration:underline; color:white;">
-      <?php if ($lang === "en") { echo "Privacy Policy";} else { echo "Aviso de privacidad";} ?>
-      </a></div>
-      <div><a href="?page=terminos" style="text-decoration:underline; color:white;">
-      <?php if ($lang === "en") { echo "Terms and conditions";} else { echo "Términos y condiciones";} ?>
-      </a></div>
-      <div><a href="mailto:ivanmet@ivanmet.com.ar" style="text-decoration:underline; color:white;">
-      <?php if ($lang === "en") { echo "Submit your CV";} else { echo "Envianos tu CV";} ?>
-      </a></div>
+        <div><a href="?page=privacidad" style="text-decoration:underline; color:white;">
+                <?php if ($lang === "en") {
+                    echo "Privacy Policy";
+                } else {
+                    echo "Aviso de privacidad";
+                } ?>
+            </a></div>
+        <div><a href="?page=terminos" style="text-decoration:underline; color:white;">
+                <?php if ($lang === "en") {
+                    echo "Terms and conditions";
+                } else {
+                    echo "Términos y condiciones";
+                } ?>
+            </a></div>
+        <div><a href="mailto:ivanmet@ivanmet.com.ar" style="text-decoration:underline; color:white;">
+                <?php if ($lang === "en") {
+                    echo "Submit your CV";
+                } else {
+                    echo "Envianos tu CV";
+                } ?>
+            </a></div>
     </div>
     <div class="footer-contact" style="color:White;">
         <img src="./src/LOGO FINAL CURVAS.png" class="contactLogo">
         <div>Maipú 859, Piso 3</div>
-        <div>CP C1006ACK, Capital Federal<br/>Buenos Aires, Argentina</div>
-        <div>+54 11 4311 0784</div>
+        <div>CP C1006ACK, Capital Federal<br />Buenos Aires, Argentina</div>
+        <div style="color: #fff">+54 11 4311 0784</div>
         <div><a href="mailto:ivanmet@ivanmet.com.ar" style="color:white; text-decoration:underline;">ivanmet@ivanmet.com.ar</a></div>
     </div>
     <div class="footer-contact" style="color:White;"><img src="./src/LOGO FINAL CURVAS Chile.png" class="contactLogo">
         <div>Haydeé Arias 769</div>
-        <div>Pobl. Portal del Estrecho<br/>Punta Arenas, Chile</div>
+        <div>Pobl. Portal del Estrecho<br />Punta Arenas, Chile</div>
         <div>+56 61 221 0562</div>
         <div><a href="mailto:administracion@ivanmetchile.com" style="color:white; text-decoration:underline;">administracion@ivanmetchile.com</a></div>
 
